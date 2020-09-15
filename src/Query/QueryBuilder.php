15cc967ccb458f47d31f6da90378fa79ea3c9f4e
Raymond Kiekens <raymond@oberon.nl>
2020-09-15 14:40:27 +0200

    removed custominfix

--- src/Query/QueryBuilder.php
+++ src/Query/QueryBuilder.php
@@ -167,7 +167,6 @@ class QueryBuilder
                         'id',
                         'locale',
                         'customerFirstName',
-                        'customerInfix',
                         'customerLastName',
                         'customerPhoneNumber',
                         'customerEmail',
